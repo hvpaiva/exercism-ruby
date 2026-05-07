7d9521d62c7bc16503b332c01de24383f756fe56
Highlander Paiva <highlander.paiva@rdstation.com>
2026-05-07 08:59:10 -0300

    Remove synced Rust solutions

--- solutions/rust/gigasecond/2/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use time::{PrimitiveDateTime as DateTime, ext::NumericalDuration};
-
-// Returns a DateTime one billion seconds after start.
-pub fn after(start: DateTime) -> DateTime {
-    start.saturating_add(1e9.seconds())
-}
